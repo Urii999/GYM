cc11b14b0f066acb6f47925dbec2d29ba5e5c1a7
Urii999 <56130256+Urii999@users.noreply.github.com>
2019-12-03 18:45:14 +0100

    adding R

--- app/src/main/java/com/uriel/gym/MainActivity.java
+++ app/src/main/java/com/uriel/gym/MainActivity.java
@@ -3,6 +3,8 @@ package com.uriel.gym;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mygym.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
