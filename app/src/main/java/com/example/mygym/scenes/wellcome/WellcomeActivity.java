4fb2bd579602be0182a72c3513df1bff270462f8
Urii999 <56130256+Urii999@users.noreply.github.com>
2019-12-12 19:05:35 +0100

    Button Inicia

--- app/src/main/java/com/example/mygym/scenes/wellcome/WellcomeActivity.java
+++ app/src/main/java/com/example/mygym/scenes/wellcome/WellcomeActivity.java
@@ -2,9 +2,13 @@ package com.example.mygym.scenes.wellcome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mygym.R;
+import com.example.mygym.scenes.exercises.ExercisesActivity;
 
 public class WellcomeActivity extends AppCompatActivity {
 
@@ -12,5 +16,17 @@ public class WellcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wellcome);
+
+            Button btn = (Button) findViewById(R.id.button);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), ExercisesActivity.class);
+                    startActivityForResult(intent, 0);
+                }
+            });
+        }
+
+
     }
-}
+
